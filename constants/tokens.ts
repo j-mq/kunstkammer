1c39e36ddb100df326611596c7311beab9856cb8
Jacobo <jacobo@fixel.co.jp>
2023-01-02 11:33:37 +0900

    prototype UI

---
+++ constants/tokens.ts
@@ -0,0 +1,22 @@
+export const theme = {
+  backgroundLighter: '#808080',
+  backgroundLight: '#333333',
+  backgroundDark: '#1B1B1B',
+  //
+  labelColor: '#FFFFFF',
+  notSelected: '#C0C0C0',
+  //
+  inputBackground: '#B3B3B3',
+  inputBackgroundFaded: 'rgba(179, 179, 179, 0.75)',
+  inputBorder: '2px solid #343434',
+  inputBorderActive: '2px solid #D86EBA',
+  inputTextColor: '#1A1A1A',
+  inputSelectedColor: '#D86EBA',
+  inputMenuBackground: '#E7E7E7',
+  inputMenuHover: '#E9C5DF',
+  //
+  editorBackground: '#4C4C4C',
+  //
+  dropShadow: '0px 3px 3px #1a1a1a',
+  selectionArea: '#666666',
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { theme } from '../constants/tokens';
+import { ThemeProvider } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
